3368918cb6126c50890d4075b191af7397598b1e
singla1209 <arvind.singla12@gmail.com>
2026-07-26 21:26:26 +0530

    Add files via upload

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -4,7 +4,6 @@ import {
     signOut
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-
 const menuLogin = document.getElementById("menuLogin");
 const menuRegister = document.getElementById("menuRegister");
 const menuLogout = document.getElementById("menuLogout");
@@ -12,21 +11,17 @@ const logoutBtn = document.getElementById("logoutBtn");
 
 onAuthStateChanged(auth, (user) => {
 
-    console.log("User =", user);
-
-    alert(user ? "USER IS LOGGED IN" : "USER IS LOGGED OUT");
-
     if (user) {
 
-        if (menuLogin) menuLogin.style.display = "none";
-        if (menuRegister) menuRegister.style.display = "none";
-        if (menuLogout) menuLogout.style.display = "block";
+        if(menuLogin) menuLogin.style.display = "none";
+        if(menuRegister) menuRegister.style.display = "none";
+        if(menuLogout) menuLogout.style.display = "block";
 
     } else {
 
-        if (menuLogin) menuLogin.style.display = "block";
-        if (menuRegister) menuRegister.style.display = "block";
-        if (menuLogout) menuLogout.style.display = "none";
+        if(menuLogin) menuLogin.style.display = "block";
+        if(menuRegister) menuRegister.style.display = "block";
+        if(menuLogout) menuLogout.style.display = "none";
 
     }
 
