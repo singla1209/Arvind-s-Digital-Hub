6784b1509b12fc7202b2658e9a17dcb43f9cdfad
singla1209 <arvind.singla12@gmail.com>
2026-07-28 19:37:46 +0530

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabaseUrl = "https://evmlvpcnzgufvfoxqcid.supabase.co";
 
-const supabaseKey = "YOUR_PUBLISHABLE_KEY";
+const supabaseKey = "sb_publishable_AV0IPwKlo_BICRrbN_4egA_9XBxi_Jf";
 
 export const supabase = createClient(
     supabaseUrl,
