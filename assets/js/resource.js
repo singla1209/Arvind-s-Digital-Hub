c7f3038bf41b2f75afe4ae653e73c1aa3a97717b
singla1209 <arvind.singla12@gmail.com>
2026-07-30 23:10:18 +0530

    Update resource.js

--- assets/js/resource.js
+++ assets/js/resource.js
@@ -1,1 +1,30 @@
+/*==================================================
+ARVIND DIGITAL HUB
+assets/js/resource.js
+==================================================*/
+
 console.log("Resource Details Page Loaded");
+
+/*====================================
+GET RESOURCE ID FROM URL
+====================================*/
+
+const params = new URLSearchParams(window.location.search);
+
+const resourceId = params.get("id");
+
+console.log("Resource ID:", resourceId);
+
+/*====================================
+CHECK URL
+====================================*/
+
+if (!resourceId) {
+
+    document.getElementById("resourceTitle").textContent =
+        "Resource Not Found";
+
+    document.getElementById("resourceDescription").textContent =
+        "No resource ID was supplied in the URL.";
+
+}
